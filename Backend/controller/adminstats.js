7cf64c465d294b43342dfa5ea5fd840d78b09be7
Anubhav Raj <anubhavr219@gmail.com>
2025-07-17 14:52:48 +0530

    backend working

--- Backend/controller/adminstats.js
+++ Backend/controller/adminstats.js
@@ -4,21 +4,33 @@ const Submissions = require("../schema/submission");
 
 const userStats = async(req,res)=>{
 try {
+    const adminId = req.result._id
+
     const totalUsers = await User.countDocuments();
-    // const totalProblems = await Problem.countDocuments();
     const [easyprob, mediumprob, hardprob] = await Promise.all([
       Problem.countDocuments({ difficulty: "easy" }),
       Problem.countDocuments({ difficulty: "medium" }),
       Problem.countDocuments({ difficulty: "hard" }),
     ]);
     const totalSubmissions = await Submissions.countDocuments();
+
+    const admin = await User.findById(adminId)
+    const adminSince = admin.createdAt.toLocaleDateString('en-IN', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    });
+    const admin_problems = await Problem.countDocuments({problemCreator:adminId})
+
     res.json({
         totalUsers,
         easyprob,
         mediumprob,
         hardprob,
         totalProblems:easyprob+mediumprob+hardprob,
-        totalSubmissions
+        totalSubmissions,
+        adminSince,
+        admin_problems
     })
 } catch (err) {
   console.error("Error:", err);
