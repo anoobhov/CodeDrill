1d60603149b6f64fea7e73f09e8c63beb4010717
Anubhav Raj <anubhavr219@gmail.com>
2025-07-28 08:04:19 +0530

    hosting error

--- Frontend/src/utils/axiosClient.js
+++ Frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosClient =  axios.create({
-    baseURL: 'https://codedrill-c3ki.onrender.com/',
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,//token bhi bhejna
     headers: {
         'Content-Type': 'application/json'
