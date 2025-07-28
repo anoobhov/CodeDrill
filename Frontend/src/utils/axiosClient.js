88a45b4b8cf09c18abdd2764eeb04d6cc277f250
Anubhav Raj <anubhavr219@gmail.com>
2025-07-28 07:50:35 +0530

    hosting

--- Frontend/src/utils/axiosClient.js
+++ Frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosClient =  axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'http://https://codedrill-c3ki.onrender.com/',
     withCredentials: true,//token bhi bhejna
     headers: {
         'Content-Type': 'application/json'
